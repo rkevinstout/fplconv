cc56e3fc9b356155239553eac936f0e73f760987
Kevin Stout <rkevinstout@gmail.com>
2023-08-29 11:57:50 -0500

    refactor: move FlightPlan property assignments to ctor

--- src/fplconv/XPlane/FlightPlan.cs
+++ src/fplconv/XPlane/FlightPlan.cs
@@ -1,20 +1,42 @@
 ﻿namespace fplconv.XPlane;
 
+/// <summary>
+/// Represents the components of a flight plan as described
+/// in the x-plane documentation
+/// </summary>
+/// <see cref="https://developer.x-plane.com/article/flightplan-files-v11-fms-file-format/"/> 
 public class FlightPlan
 {   
-    public string? Name { get; set; }
+    public FlightPlan(string name, Waypoint[] waypoints)
+    {
+        Name = name;
+        Route = waypoints;
+    }
+    public string? Name { get; }
 
     public Waypoint Departure => Route.First();
 
     public Waypoint Destination => Route.Last();
 
-    public Waypoint[] Route {get;set;} = Array.Empty<Waypoint>();
+    public Waypoint[] Route { get; }
 
     public class Waypoint
     {
+        public Waypoint(
+            string identifier, 
+            WaypointType type,
+            decimal latitude,
+            decimal longitude
+            )
+        {
+            Identifier = identifier;
+            Type = type;
+            Latitude = latitude;
+            Longitude = longitude;            
+        }
+
         public enum WaypointType
         {
-            Undfined =0,
             Airport = 1,
             NDB = 2,
             VOR = 3,
@@ -32,7 +54,7 @@ public class FlightPlan
 
         public WaypointType Type { get; set; }
 
-        public string? Identifier { get; set; }
+        public string Identifier { get; set; }
 
         public LegType Via { get; set; } = LegType.Direct;
 
